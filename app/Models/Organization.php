bc5f6f1b8899bc3984964277979d9356998a6603
aminevg <38422328+aminevg@users.noreply.github.com>
2022-12-18 15:02:03 +0000

    refactor: add fillable fields to Organization model

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -14,6 +14,17 @@ class Organization extends Model
 {
     use HasFactory, SoftDeletes;
 
+    protected $fillable = [
+        "name",
+        "email",
+        "phone",
+        "address",
+        "city",
+        "region",
+        "country",
+        "postal_code",
+    ];
+
     /** @return BelongsTo<Account,Organization>  */
     public function account(): BelongsTo
     {
