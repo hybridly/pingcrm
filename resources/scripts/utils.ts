81c6747212854d38f380115cfd4d8ed31bdc5419
aminevg <38422328+aminevg@users.noreply.github.com>
2022-12-19 10:24:29 +0000

    feat: pickDefinedValues now accepts ref objects

--- resources/scripts/utils.ts
+++ resources/scripts/utils.ts
@@ -15,10 +15,15 @@ export function mapValuesToNull<T extends object>(
     );
 }
 
-export function pickDefinedValues<T extends object>(object: T): Partial<T> {
-    return Object.entries(object).reduce((previousValue, [key, value]) => {
-        if (value)
-            previousValue = Object.assign(previousValue, { [key]: value });
-        return previousValue;
-    }, {} as Partial<T>);
+export function pickDefinedValues<T extends object>(
+    object: T | Ref<T>
+): Partial<T> {
+    return Object.entries(unref(object)).reduce(
+        (previousValue, [key, value]) => {
+            if (value)
+                previousValue = Object.assign(previousValue, { [key]: value });
+            return previousValue;
+        },
+        {} as Partial<T>
+    );
 }
