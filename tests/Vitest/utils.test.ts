660516a37e81798595d515c7dd8a304998c02853
aminevg <38422328+aminevg@users.noreply.github.com>
2022-12-19 10:24:50 +0000

    test: add unit tests for pickDefinedValues

--- tests/Vitest/utils.test.ts
+++ tests/Vitest/utils.test.ts
@@ -26,3 +26,24 @@ describe("mapValuesToNull", () => {
         expect(actual).toStrictEqual({});
     });
 });
+
+describe("pickDefinedValues", () => {
+    const object = { a: 1, b: undefined, c: null, d: "", e: 0 };
+    const refObject = ref({ a: 1, b: undefined, c: null, d: "", e: 0 });
+
+    it("should map all values to null", () => {
+        const actual = pickDefinedValues(object);
+        expect(actual).toStrictEqual({ a: 1 });
+    });
+
+    it("should return identical results when passing an object ref versus a plain object", () => {
+        const plainActual = pickDefinedValues(object);
+        const refActual = pickDefinedValues(refObject);
+        expect(refActual).toStrictEqual(plainActual);
+    });
+
+    it("should return an empty object when passing an empty object", () => {
+        const actual = pickDefinedValues({});
+        expect(actual).toStrictEqual({});
+    });
+});
