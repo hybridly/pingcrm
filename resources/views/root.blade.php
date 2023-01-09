942589163ce63c16730b0f4bfa3bf61617aba4f2
aminevg <38422328+aminevg@users.noreply.github.com>
2023-01-09 10:36:43 +0000

    feat: add favicon

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -3,6 +3,7 @@
 	<head>
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 			@vite('resources/application/main.ts')
 	</head>
 	<body class="font-sans leading-none text-gray-700 antialiased">
